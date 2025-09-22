9ebc663b6408380012a35b4e9fad397c9fbe8354
Simit Modi <simitjmodi@gmail.com>
2025-09-22 20:09:40 +0000

    File changes

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyYOUR_API_KEY",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "your-sender-id",
-  appId: "your-app-id",
+  "projectId": "studio-5920023951-be7cb",
+  "appId": "1:863254815612:web:fec4d4ad644ab284c1faf9",
+  "apiKey": "AIzaSyBn6TEkFB8SAi3eL_-yfsGGQMhq2P0gODo",
+  "authDomain": "studio-5920023951-be7cb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "863254815612"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
