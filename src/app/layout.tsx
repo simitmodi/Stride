7b16fddd300b5fef0a7d71926ce5babfca10ed1b
Simit Modi <simitjmodi@gmail.com>
2025-09-23 17:14:01 +0000

    <head>   <script src="https://www.google.com/recaptcha/enterprise.js?ren

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { ScrollAwareFooter } from '@/components/scroll-aware-footer';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Stride',
@@ -19,6 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+        <Script src="https://www.google.com/recaptcha/enterprise.js?render=6Lfqw9IrAAAAAATsZvi3VG5KnxYHZWZA7eap6url" strategy="head" />
       </head>
       <body className="font-body antialiased h-full flex flex-col">
         <div className="flex-grow">
