927065edd152f58ad212f15acaaf4d80cb000bef
Simit Modi <simitjmodi@gmail.com>
2025-09-23 13:05:43 +0000

    Add T&C button to this (_for element <Footer>_)

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -41,6 +41,14 @@ export default function Footer({ className }: { className?: string }) {
                 href="#"
                 className="inline-block transform rounded-md px-3 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:bg-white hover:text-black"
               >
+                T&amp;C
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#"
+                className="inline-block transform rounded-md px-3 py-2 transition-all duration-300 ease-in-out hover:scale-105 hover:bg-white hover:text-black"
+              >
                 Contact
               </Link>
             </li>
