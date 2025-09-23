96d619c3c4c06d3e2ba4c54079df5f7453e32f13
Simit Modi <simitjmodi@gmail.com>
2025-09-23 17:56:08 +0000

    this implementation is good just you have to remove that old part

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -31,9 +31,9 @@ function Calendar({
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption_label: "hidden",
         caption_dropdowns: "flex justify-center gap-1",
-        nav: "space-x-1 flex items-center",
+        nav: "hidden",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
           "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100"
