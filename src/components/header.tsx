24343015578924c835b2bcb6cfd2da767bcc1d46
Simit Modi <simitjmodi@gmail.com>
2025-09-24 21:11:33 +0000

    header and calendar strip color are looking different calendar strip hav

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -54,7 +54,7 @@ export default function Header() {
 
 
   return (
-    <header className="sticky top-0 z-30 flex h-16 items-center gap-4 border-b bg-sidebar px-4 md:px-6">
+    <header className="sticky top-0 z-30 flex h-16 items-center gap-4 border-b bg-card px-4 md:px-6">
       <nav className="flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 w-full">
         <Link
           href="/dashboard/customer"
