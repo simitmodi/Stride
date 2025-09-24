696eff3137298225b9552962fb8f1a4a07b2f40c
Simit Modi <simitjmodi@gmail.com>
2025-09-24 21:18:19 +0000

    remove name of the month from pop up calendar

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -31,7 +31,7 @@ function Calendar({
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption_label: "hidden",
         caption_dropdowns: "flex justify-center gap-1",
         nav: "flex items-center",
         nav_button: cn(
