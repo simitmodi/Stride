abe555a0f9bdbf92189d9f9f751d3b6129b82028
Simit Modi <simitjmodi@gmail.com>
2025-09-26 22:52:38 +0000

    Try fixing this error: `Runtime Error: Error: showConfirmemo is not defi

--- src/components/auth/customer-signup-form.tsx
+++ src/components/auth/customer-signup-form.tsx
@@ -267,7 +267,7 @@ export function CustomerSignUpForm() {
                   className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
                   onClick={() => setShowConfirmPassword((prev) => !prev)}
                 >
-                  {showConfirmemo ? (
+                  {showConfirmPassword ? (
                     <EyeOff className="h-4 w-4" aria-hidden="true" />
                   ) : (
                     <Eye className="h-4 w-4" aria-hidden="true" />
